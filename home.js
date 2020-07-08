399febe0df63e905db7e212e16f9cf8e3c3d1387
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-07-08 22:31:46 +0100

    New changes

--- home.js
+++ home.js
@@ -1,5 +1,15 @@
+let key = 42;
+
 function getSecretCode(value) {
-    let code = value * 42;
+
+    let keyGenerator = function() {
+        let key = 12;
+        console.log('in keyGenerator:', key);
+        return key;
+    }
+
+    let code = value * keyGenerator();
+    console.log('in getSecretCode', key);
     return code;
 }
 
