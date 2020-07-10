595ba45c7678791faf9b7d4e5402ae96f47529e7
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-07-10 15:21:06 +0100

    Objects contd..

--- home.js
+++ home.js
@@ -1,10 +1,1 @@
-let person = {
-    name:'John',
-    age: 32,
-    partTime: false,
-    showInfo: function(realAge){
-        showMessage(this.name + ' is ' + realAge);
-    }
 
-};
-showMessage( typeof person.showInfo );
