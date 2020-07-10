0b3159b22df5dcf2d50fa3e80368066b590afd89
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-07-10 14:54:18 +0100

    Learning about Objects

--- home.js
+++ home.js
@@ -1,18 +1,10 @@
-let key = 42;
-
-function getSecretCode(value) {
-
-    let keyGenerator = function() {
-        let key = 12;
-        console.log('in keyGenerator:', key);
-        return key;
+let person = {
+    name:'John',
+    age: 32,
+    partTime: false,
+    showInfo: function(realAge){
+        showMessage(this.name + ' is ' + realAge);
     }
 
-    let code = value * keyGenerator();
-    console.log('in getSecretCode', key);
-    return code;
-}
-
-let SecretCode = getSecretCode(2);
-showMessage( SecretCode );
-
+};
+showMessage( typeof person.showInfo );
